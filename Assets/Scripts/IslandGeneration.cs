28a5b11fc7f5fd180396932d39cf3b75db666340
Wendy Shi <66499583+ws1468@users.noreply.github.com>
2022-04-13 23:32:30 -0400

    trees

--- Assets/Scripts/IslandGeneration.cs
+++ Assets/Scripts/IslandGeneration.cs
@@ -5,32 +5,37 @@ using UnityEngine.SceneManagement;
 
 /*
  goes on the world object
- generates the terrain of the world 
+ generates the terrain of the world
  */
 
 public class IslandGeneration : MonoBehaviour
 {
-    // the world 
+    // the world
     int sizeX = 100;
     int sizeY = 100;
     Cell[,] grid; // array of cells will make up this grid (the world is represented by this grid)
 
-    // tiles/items that will be used spawned 
+    // tiles/items that will be used spawned
     public GameObject grassTile;
     public GameObject port;
     public GameObject gem;
 
     public GameObject player;
 
+    public GameObject[] treePrefabs; //TRYINGOUTTTTTT PUT TREEESSS INNNN
+
     int seed;
     float xOffSet;
     float yOffSet;
 
-    public float scale = 0.1f; // scale for the perlin noise 
+    public float scale = 0.1f; // scale for the perlin noise
     public float terDetail;
     public float terHeight;
-    public int waterHeight; // this is the highest level of water 
+    public int waterHeight; // this is the highest level of water
     public float waterLevel = 0.4f; // any noise value over this number is not water and any noise value under this is water
+
+    public float treeNoiseScale = .1f;  //TRYINGOUTTTTTT PUT TREEESSS INNNN
+    public float treeDensity = .001f;   //TRYINGOUTTTTTT PUT TREEESSS INNNN
 
     int sceneNumber;
 
@@ -43,7 +48,7 @@ public class IslandGeneration : MonoBehaviour
     void Start()
     {
         player.SetActive(false);
-        SetSeedAndOffsets(); // set the seeds and offsets for each island (so we can get the same map each time for each island) 
+        SetSeedAndOffsets(); // set the seeds and offsets for each island (so we can get the same map each time for each island)
         GenerateTerrain();
         SpawnPort();
         SpawnPlayer();
@@ -52,7 +57,7 @@ public class IslandGeneration : MonoBehaviour
 
     void SetSeedAndOffsets()
     {
-        if(sceneNumber == 0) // Island 1 
+        if(sceneNumber == 0) // Island 1
         {
             xOffSet = (float) -7100.375;
             yOffSet = (float) 9689.953;
@@ -63,7 +68,7 @@ public class IslandGeneration : MonoBehaviour
             //by adding random offsets to the noise map, we will generate a new map every time
             xOffSet = Random.Range(-10000f, 10000f);
             yOffSet = Random.Range(-10000f, 10000f);
-            
+
             Debug.Log(xOffSet);
             Debug.Log(yOffSet);
             Debug.Log(seed);
@@ -85,7 +90,7 @@ public class IslandGeneration : MonoBehaviour
             }
         }
 
-        // initialize a grid with cells 
+        // initialize a grid with cells
         grid = new Cell[sizeX, sizeY];
         for (int y = 0; y < sizeY; y++)
         {
@@ -97,24 +102,48 @@ public class IslandGeneration : MonoBehaviour
                 if (noiseValue < waterLevel && z < waterHeight)
                 {
                     cell.isWater = true;
-                    cell.height = 0; 
+                    cell.height = 0;
                 }
                 else
                 {
                     cell.isWater = false;
-                    cell.height = z; 
+                    cell.height = z;
                     grassTile = Instantiate(grassTile, new Vector3(x, z, y), Quaternion.identity);
                 }
                 grid[x, y] = cell;
             }
         }
-        
+
+//TRYINGOUTTTTTT PUT TREEESSS INNNN
+        for(int y = 0; y < sizeY; y++) {
+            for(int x = 0; x < sizeX; x++) {
+                Cell cell = grid[x, y];
+                if(!cell.isWater) {
+                    float v = Random.Range(0f, treeDensity);
+                    if(noiseMap[x, y] < v) {
+                        GameObject prefab = treePrefabs[Random.Range(0, treePrefabs.Length)];
+                        GameObject tree = Instantiate(prefab, transform);
+                        tree.transform.position = new Vector3(x, cell.height+1, y);
+                        tree.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360f), 0);
+                        tree.transform.localScale = Vector3.one * Random.Range(.2f, 1f);
+                    }
+                }
+            }
+        }
+//TRYINGOUTTTTTT PUT TREEESSS INNNN
 
     }
 
+
+
+
+
+
+
+
     void SpawnPort()
     {
-        if(sceneNumber == 0) // Island 1 
+        if(sceneNumber == 0) // Island 1
         {
             port = Instantiate(port, new Vector3(10, 2, 99), Quaternion.identity);
             port = Instantiate(port, new Vector3(12, 2, 99), Quaternion.identity);
