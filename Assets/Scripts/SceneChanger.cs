0e3fde3f5b150da8125cc2e4ba11b3350d0099ea
jh6338 <60440293+jh6338@users.noreply.github.com>
2022-04-28 13:04:31 -0400

    switching scenes using 1 & 2
    switch islands by pressing 1 or 2. only for presentation & debugging purposes

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     public KeyCode restart = KeyCode.R; 
+    public KeyCode island1 = KeyCode.Alpha1; 
+    public KeyCode island2 = KeyCode.Alpha2; 
+
+
 
     // Update is called once per frame
     void Update()
@@ -15,6 +19,16 @@ public class SceneChanger : MonoBehaviour
             Debug.Log("Pressed R key to restart.");
             Scene curr_scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(curr_scene.name);
+        }
+
+        if (Input.GetKeyDown(island1)) {
+            Debug.Log("Pressed 1, to switch to island 1.");
+            SceneManager.LoadScene(0); 
+        }
+
+        if (Input.GetKeyDown(island2)) {
+            Debug.Log("Pressed 2, to switch to island 2."); 
+            SceneManager.LoadScene(1); 
         }
     }
 }
