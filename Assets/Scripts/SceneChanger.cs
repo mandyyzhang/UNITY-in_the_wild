4b38446d82af0d14a5e88476db1a0aaa8bebb990
jh6338 <60440293+jh6338@users.noreply.github.com>
2022-04-28 15:34:09 -0400

    Revert "added load scene"
    This reverts commit 0d25f3acdb0980a044a38970dede36fd02c46b43.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -9,34 +9,26 @@ public class SceneChanger : MonoBehaviour
     public KeyCode island1 = KeyCode.Alpha1; 
     public KeyCode island2 = KeyCode.Alpha2; 
 
-    private Scene curr_scene; 
 
-    // Note: to see what index the scene is at, look at the build settings 
-    void Awake() {
-        curr_scene = SceneManager.GetActiveScene(); 
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (curr_scene.name == "Load" || curr_scene.buildIndex == 0) {
-            return; 
-        }
-        
         if (Input.GetKeyDown(restart))
         {
             Debug.Log("Pressed R key to restart.");
+            Scene curr_scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(curr_scene.name);
         }
 
         if (Input.GetKeyDown(island1)) {
             Debug.Log("Pressed 1, to switch to island 1.");
-            SceneManager.LoadScene(1); 
+            SceneManager.LoadScene(0); 
         }
 
         if (Input.GetKeyDown(island2)) {
             Debug.Log("Pressed 2, to switch to island 2."); 
-            SceneManager.LoadScene(2); 
+            SceneManager.LoadScene(1); 
         }
     }
 }
