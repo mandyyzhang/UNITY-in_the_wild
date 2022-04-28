be26235e785602a0be200c0e708e8aa4f3d8980a
jh6338 <60440293+jh6338@users.noreply.github.com>
2022-04-28 15:45:45 -0400

    press 1 & 2 to switch between islands

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,6 +6,16 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     public KeyCode restart = KeyCode.R; 
+    public KeyCode island1 = KeyCode.Alpha1; 
+    public KeyCode island2 = KeyCode.Alpha2; 
+
+    private Scene curr_scene; 
+
+    // Note: Check build settings to see which index the scene is at. 
+
+    void Awake() {
+        curr_scene = SceneManager.GetActiveScene(); 
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,8 +23,15 @@ public class SceneChanger : MonoBehaviour
         if (Input.GetKeyDown(restart))
         {
             Debug.Log("Pressed R key to restart.");
-            Scene curr_scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(curr_scene.name);
+        }
+        if (Input.GetKeyDown(island1)) {
+            Debug.Log("Pressed 1 to go to island 1."); 
+            SceneManager.LoadScene(0); 
+        }
+        if (Input.GetKeyDown(island2)) {
+            Debug.Log("Pressed 2 to go island 2."); 
+            SceneManager.LoadScene(1);
         }
     }
 }
